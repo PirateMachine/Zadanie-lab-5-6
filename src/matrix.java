563a6e88735b5f50a95e68e220758aeae0c0f827
PirateMachine <Wiki20221@gmail.com>
2024-12-04 16:09:08 +0100

    git commit

---
+++ src/matrix.java
@@ -0,0 +1,21 @@
+public class matrix {
+
+        public static void main(String[] args) {
+            int[][] matrix = new int[10][10];
+            for (int i = 0; i < 10; i++) {
+                matrix[i][i] = i;
+            }
+            int diagonalSum = 0;
+            for (int i = 0; i < 10; i++) {
+                diagonalSum += matrix[i][i];
+            }
+            System.out.println();
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("\nSum: " + diagonalSum);
+        }
+    }
